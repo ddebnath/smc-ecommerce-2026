eee88ea54f826a25199bf410d2ea3ea76b13eea5
ddebnath <d.debnath@smcs.ac.in>
2026-04-07 23:59:12 +0530

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,7 @@ dotenv.config();
 // Middleware to parse JSON bodies in requests
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://ddebnath-smc-ecommerce-2026.vercel.app",
-    ],
-
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
