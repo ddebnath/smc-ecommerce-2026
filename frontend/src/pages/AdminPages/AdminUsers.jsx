77aef179edd8dfbff39acccaa6c90c69f53c9b3e
ddebnath <d.debnath@smcs.ac.in>
2026-04-29 03:01:44 +0530

    added dashboard with data analytics

--- frontend/src/pages/AdminPages/AdminUsers.jsx
+++ frontend/src/pages/AdminPages/AdminUsers.jsx
@@ -85,10 +85,46 @@ const AdminUsers = () => {
       user.email?.toLowerCase().includes(search.toLowerCase()),
   );
 
+  // user statistics
+  const stats = {
+    totalUsers: users.length,
+
+    admins: users.filter((u) => u.role === "admin").length,
+
+    productOwners: users.filter((u) => u.role === "productOwner").length,
+
+    normalUsers: users.filter((u) => u.role === "user").length,
+  };
+
   return (
     <div className="pl-[350px] py-20 mx-auto px-6">
-      <h1 className="text-2xl font-bold mb-6">All Users</h1>
+      <h1 className="text-2xl font-bold mb-10 text-center">All Users</h1>
+      {/* ================= USER STATS ================= */}
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
+        <div className="bg-white shadow rounded-xl p-4 text-center border">
+          <p className="text-sm text-gray-500">Total Users</p>
+          <p className="text-xl font-bold">{stats.totalUsers}</p>
+        </div>
 
+        <div className="bg-blue-50 border border-blue-200 rounded-xl p-4 text-center">
+          <p className="text-sm text-blue-600">Admins</p>
+          <p className="text-xl font-bold text-blue-700">{stats.admins}</p>
+        </div>
+
+        <div className="bg-green-50 border border-green-200 rounded-xl p-4 text-center">
+          <p className="text-sm text-green-600">Product Owners</p>
+          <p className="text-xl font-bold text-green-700">
+            {stats.productOwners}
+          </p>
+        </div>
+
+        <div className="bg-yellow-50 border border-yellow-200 rounded-xl p-4 text-center">
+          <p className="text-sm text-yellow-600">Users</p>
+          <p className="text-xl font-bold text-yellow-700">
+            {stats.normalUsers}
+          </p>
+        </div>
+      </div>
       {/* 🔍 Search Box */}
       <div className="flex items-center relative mb-6">
         <SearchIcon className="absolute left-2 text-gray-600" />
